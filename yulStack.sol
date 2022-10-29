5f724f26b7d28198154e07cb4473c02f29b65cbd
GregTheDev <40359730+gcurtis98@users.noreply.github.com>
2022-10-29 18:57:28 -0500

    Update YulStack

--- yulStack.sol
+++ yulStack.sol
@@ -1,84 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.15;
 
-contract Stack {
+contract uuintStack {
+    
+    error IndexOutOfBounds();
     uint256[] internal stack;
 
-    //Push new item on to the stack.
+    //Pushes a new item on to the stack.
+    //Array length will overflow if it exceeds 2^256-1.
     function stackPush(uint256 item) external {
-        //stack.push(item);
         assembly {
-            let freeMemoryPointer := mload(0x40)
-            mstore(freeMemoryPointer, stack.slot)
             let size := sload(stack.slot)
             sstore(stack.slot, add(size, 1))
-            sstore(add(keccak256(freeMemoryPointer, 0x20), size), item)
+            mstore(0x00, stack.slot)
+            sstore(add(keccak256(0x00, 0x20), size), item)
         }
     }
-    //Remove last item added to the stack.
+
+    //Removes the last item added to the stack.
+    //Will revert IndexOutOfBounds() if the stack is empty.
     function stackPop() external {
-        //stack.pop();
         assembly {
             let size := sload(stack.slot)
             if iszero(size) {
-                revert(0, 0)
+                mstore(0x00, 0x4e23d035)
+                revert(0x1c, 0x04)
             }
             sstore(stack.slot, sub(size, 1))
         }
     }
+
     //Returns the amount of items in the stack.
     function stackSize() external view returns (uint256 ret) {
-        //stack.length
         assembly {
             ret := sload(stack.slot)
         }
     }
 
-    //Will revert if index out of bounds, use pop ito grow stack.
+    //Set the item at 'index' to 'value'.
+    //Will revert IndexOutOfBounds() if the index is out of bounds, use push to grow the stack.
     function setStackIndex(uint256 index, uint256 value) external {
-        //stack[index] = value;
         assembly {
-            let size := sload(stack.slot)
-            if iszero(lt(index, size)) {
-                revert(0, 0)
+            if iszero(lt(index, sload(stack.slot))) {
+                mstore(0x00, 0x4e23d035)
+                revert(0x1c, 0x04)
             }
-            let freeMemoryPointer := mload(0x40)
-            mstore(freeMemoryPointer, stack.slot)
-            sstore(add(keccak256(freeMemoryPointer, 0x20), index), value)
+            mstore(0x00, stack.slot)
+            sstore(add(keccak256(0x00, 0x20), index), value)
         }
     }
 
-    //Get item at index of stack.
+    //Get item at specified 'index' of the stack.
+    //Will revert IndexOutOfBounds() if the index is out of bounds.
     function readStackIndex(uint256 index) external view returns (uint256 ret) {
-        // return stack[index];
         assembly {
-            let size := sload(stack.slot)
-            if iszero(lt(index, size)) {
-                revert(0, 0)
+            if iszero(lt(index, sload(stack.slot))) {
+                mstore(0x00, 0x4e23d035)
+                revert(0x1c, 0x04)
             }
-            let freeMemoryPointer := mload(0x40)
-            mstore(freeMemoryPointer, stack.slot)
-            ret := sload(add(keccak256(freeMemoryPointer, 0x20), index))
+            mstore(0x00, stack.slot)
+            ret := sload(add(keccak256(0x00, 0x20), index))
         }
     }
 
-    // Finds distance from top of stack.
-    // Expensive, O(n); n = distance from top of stack
-    // Lookups deep in very large stacks may run out of gas.
-    // use IndexedStack.sol if you need to call this function from a state-mutable function.
+    // Finds distance of 'value' from the top of the stack.
+    // Returns 1 if 'value' is at the top of the stack.
+    // Returns 0 if 'value' is not found.
+    // Expensive, O(n); n = distance from top of stack.
+    // Lookups deep in large stacks may run out of gas.
     function findDistance(uint256 value) external view returns (uint256 dist) {
         assembly {
             let size := sload(stack.slot)
-            let freeMemoryPointer := mload(0x40)
             for {let i := sub(size, 1)} gt(size, i) {i := sub(i, 1)} {
-                mstore(freeMemoryPointer, stack.slot)
-                if eq(sload(add(keccak256(freeMemoryPointer, 0x20), i)), value) {
+                mstore(0x00, stack.slot)
+                if eq(sload(add(keccak256(0x00, 0x20), i)), value) {
                     dist := sub(size, i)
                     break
                 }
-            }
-            if eq(dist, 0) {
-                revert(0, 0)
             }
         }
     }
