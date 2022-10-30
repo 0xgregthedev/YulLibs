d17a2a3af5e754ab02c258cdd4ce0f4862ae5ef5
gcurtis98 <40359730+gcurtis98@users.noreply.github.com>
2022-10-29 19:59:48 -0500

    added reentrancy guard and reorganized.

---
+++ src/Utils/ReentrancyGuard.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.4;
+
+abstract contract ReentrancyGuard {
+    error Reentered();
+    uint256 private _locked = 1;
+
+    modifier nonReentrant() virtual {
+        assembly {
+            //Revert Reentered()
+            if eq(sload(_locked.slot), 0x2) {
+                mstore(0x00, 0xb5dfd9e5)
+                revert(0x1c, 0x04)
+            }
+            sstore(_locked.slot, 0x2)
+        }
+        _;
+        assembly {
+            sstore(_locked.slot, 0x1)
+        }
+    }
+}
